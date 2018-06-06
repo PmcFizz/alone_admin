33b011c94d470b8d5e1778f65ef27c64eeab9d7a
pangmengchen <mengchen_0212@foxmail.com>
2018-06-06 12:03:32 +0800

    网站首页设置

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,58 +3,27 @@
         <Row>
             <Col span="12">
             <Card :bordered="false">
-                <p slot="title" style="height: 30px">基本信息
-                    <Button style="float: right" type="primary" @click="clickModifyInfo" size="small">修改</Button>
-                </p>
+                <p slot="title" style="height: 30px">基本信息</p>
                 <Row>
                     <Col span="24" style="text-align: center">
-                    <Avatar size="large" src="https://i.loli.net/2017/08/21/599a521472424.jpg"/>
-                    <Form :label-width="100" style="margin-top: 20px">
-                        <FormItem label="名称:">
-                            <Input :disabled="isOnlyReady" v-model="baseInfo.name" placeholder="请输入名称..."></Input>
-                        </FormItem>
-                        <FormItem label="描述:">
-
-                            <Input :disabled="isOnlyReady"
-                                   type="textarea"
-                                   :autosize="{minRows: 2,maxRows: 5}" v-model="baseInfo.des"
-                                   placeholder="请输入描述...">
-                            </Input>
-                        </FormItem>
-                    </Form>
+                    <Avatar size="large" src="https://avatars1.githubusercontent.com/u/13403284?s=460&v=4"/>
+                    <h1 style="margin: 10px">师匠空间</h1>
+                    <p>此项目用于师匠后台管理,使用vue,iVew搭建,主要技术栈和框架有,vue,webpack,iView.</p>
                     </Col>
                 </Row>
             </Card>
             <Card :bordered="false">
-                <p slot="title">网站设置</p>
+                <p slot="title">网站信息</p>
                 <Row>
-                    <Col span="24" style="text-align: center">
-                    <Form :label-width="100" style="margin-top: 20px">
-                        <FormItem label="网站标题:">
-                            <Input :disabled="isOnlyReady" v-model="baseInfo.websiteTitle"
-                                   placeholder="请输入网站标题..."></Input>
-                        </FormItem>
-                        <FormItem label="网站关键字:">
-                            <Input :disabled="isOnlyReady" v-model="baseInfo.websiteKeyWord"
-                                   placeholder="请输入网站关键字..."></Input>
-                        </FormItem>
-                        <FormItem label="网站备案号:">
-                            <Input :disabled="isOnlyReady" v-model="baseInfo.websiteBeiAn"
-                                   placeholder="请输入网站备案号..."></Input>
-                        </FormItem>
-                        <FormItem label="网站版权所有:">
-                            <Input :disabled="isOnlyReady" v-model="baseInfo.websiteCopyRight"
-                                   placeholder="请输入网站版权所有..."></Input>
-                        </FormItem>
-                    </Form>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col span="12" style="text-align: center">
-                    <Button type="primary" @click="cancleModify" size="small">取消</Button>
-                    </Col>
-                    <Col span="12" style="text-align: center">
-                    <Button type="primary" @click="saveInfo" size="small">保存</Button>
+                    <Col span="24">
+                    <Col span="20">
+                    <span class="info-label">名称:</span><span class="info-data">师匠空间</span></Col>
+                    <Col span="20">
+                    <span class="info-label">关键字:</span><span class="info-data">师匠,管理端</span></Col>
+                    <Col span="20">
+                    <span class="info-label">备案号:</span><span class="info-data">师匠空间</span></Col>
+                    <Col span="20">
+                    <span class="info-label">版权所有:</span><span class="info-data">庞孟臣</span></Col>
                     </Col>
                 </Row>
             </Card>
@@ -78,20 +47,33 @@
           websiteBeiAn: '',
           websiteCopyRight: ''
         }
-      };
+      }
     },
     computed: {},
     methods: {
       // 点击修改
-      clickModifyInfo(){
+      clickModifyInfo () {
         this.isOnlyReady = false
       },
-      cancleModify(){
+      cancleModify () {
         location.reload()
       },
-      saveInfo(){
+      saveInfo () {
         console.log(this.baseInfo)
       }
     }
-  };
+  }
 </script>
+<style lang="less">
+    .info-label {
+        width: 200px;
+        display: inline-block;
+        text-align: right;
+        margin-right: 10px;
+    }
+
+    .info-data {
+        display: inline-block;
+        text-align: left;
+    }
+</style>
